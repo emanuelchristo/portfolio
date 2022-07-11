3ee021826172e2397e6d6a870d274df63b51c408
emanuelchristo <emanuelchristochris@gmail.com>
2022-07-11 19:14:52 +0530

    add css files

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
+import 'styles/variables.css'
 import 'styles/globals.css'
+import 'styles/text-styles.css'
+import 'styles/layout.css'
+import 'styles/form.css'
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />
