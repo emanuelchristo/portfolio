fcd14ca4aa4bc5cef4908482e2460e7920ae89ba
emanuelchristo <emanuelchristochris@gmail.com>
2022-07-20 01:08:21 +0530

    section header font-size reduce

--- components/common/SectionHeader.js
+++ components/common/SectionHeader.js
@@ -9,7 +9,7 @@ export default function SectionHeader({ imageUrl, tag, title, description, type 
 						<img className={styles['header-image']} src={imageUrl} />
 						<h3 className='word-heading'>{tag}</h3>
 					</div>
-					<h2 className='mt-3 mb-4'>{title}</h2>
+					<h2 className='mt-3 mb-4 sm:mt-1 sm:mb-3'>{title}</h2>
 					<p className='section-description px-4'>{description}</p>
 				</div>
 			)}
@@ -20,7 +20,7 @@ export default function SectionHeader({ imageUrl, tag, title, description, type 
 						<img className={styles['header-image']} src={imageUrl} />
 						<h3 className='word-heading'>{tag}</h3>
 					</div>
-					<h2 className='mt-3 mb-4 smaller-h2'>{title}</h2>
+					<h2 className='mt-3 mb-4 smaller-h2 sm:mt-2 sm:mb-3'>{title}</h2>
 					<p className='section-description'>{description}</p>
 				</div>
 			)}
